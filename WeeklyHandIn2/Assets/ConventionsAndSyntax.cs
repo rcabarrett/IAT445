076e362347b4bb3e7c6632e1d2250f0ea85099b7
Robin C. A. Barrett <rcbarret@sfu.ca>
2020-09-23 21:45:13 -0700

    completed weekly hand in

---
+++ WeeklyHandIn2/Assets/ConventionsAndSyntax.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConventionsAndSyntax : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.position.x);
+
+        if (transform.position.y <= 5f)
+        {
+            Debug.Log("I'm about to hit the ground");
+            
+            // This is how you comment
+            
+            /* this is how we do multiple lines of code
+             * it automatically puts the next * on the line for me
+             * */
+
+        }
+        
+    }
+
+    /* this is how you comment out code
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
---
+++ WeeklyHandIn2/Assets/IFStatements.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IFStatements : MonoBehaviour
+{
+    string spherecolour;
+    string red;
+    string green;
+    string blue;
+
+    private void Start()
+    {
+        red = System.Convert.ToString(Color.red);
+        green = System.Convert.ToString(Color.green);
+        blue = System.Convert.ToString(Color.blue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            colourtest();
+
+        spherecolour = System.Convert.ToString(GetComponent<Renderer>().material.color);
+       
+    }
+
+    void colourtest()
+    {
+        if(spherecolour == red)
+        {
+            print("The Sphere is Red");
+        }
+        else if(spherecolour == green)
+        {
+            print("The Sphere is Green");
+        }
+        else if (spherecolour == blue)
+        {
+            print("The Sphere is blue");
+        }
+        else
+        {
+            print("The Sphere has no colour");
+        }
+    }
+}
