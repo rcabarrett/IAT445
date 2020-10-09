207f50e567cf06aeb0da348a11994f7ada9705db
Robin C. A. Barrett <rcbarret@sfu.ca>
2020-10-09 00:31:27 -0700

    finished weeklyhandin4

---
+++ WeeklyHandIn4/Assets/beanmaker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beanmaker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject angrybean;
+    public GameObject dispenser;
+    public float respawn_time = 1.0f;
+
+    void Start()
+    {
+        StartCoroutine(beandispensing());
+    }
+    private void spawnbeans()
+    {
+        GameObject a = Instantiate(angrybean) as GameObject;
+        a.transform.position = new Vector3(dispenser.transform.position.x, dispenser.transform.position.y, dispenser.transform.position.z);
+
+    }
+    IEnumerator beandispensing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawn_time);
+            spawnbeans();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ WeeklyHandIn4/Assets/potisfull.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class potisfull : MonoBehaviour
+{
+    public GameObject beanspawn;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(beanspawn.gameObject);
+    }
+}
