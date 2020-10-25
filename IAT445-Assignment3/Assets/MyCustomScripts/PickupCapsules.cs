92218a9e1712c31ff78f21742106f8ac6788e2be
Robin C. A. Barrett <rcbarret@sfu.ca>
2020-10-25 01:34:32 -0700

    added capsule counter

--- IAT445-Assignment3/Assets/MyCustomScripts/PickupCapsules.cs
+++ IAT445-Assignment3/Assets/MyCustomScripts/PickupCapsules.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PickupCapsules : MonoBehaviour
 {
+    private int count;
+    public TextMeshProUGUI counter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        count = 0;
+
     }
 
     // Update is called once per frame
@@ -16,7 +20,14 @@ public class PickupCapsules : MonoBehaviour
         if (other.gameObject.CompareTag("Collectible"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
         }
         
+    }
+
+    void SetCountText()
+    {
+        counter.text = "You have " + count.ToString() + " power cores.";
     }
 }
---
+++ IAT445-Assignment3/Assets/README+ASSETCREDITS.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class README : MonoBehaviour
+{
+    //"Please refer to the MyCustomScripts Folder for Scripts that I have written";
+    
+    //"Assets used in the project include the following:";
+    //"Rock Package https://assetstore.unity.com/packages/3d/props/exterior/rock-package-118182 ";
+    //"eU Sci-Fi textures set Volume 1  https://github.com/rcabarrett/IAT445/tree/master/IAT445-Assignment3 ";
+    //"Sci-Fi Texture Pack 1 https://assetstore.unity.com/packages/2d/textures-materials/sci-fi-texture-pack-1-23301";
+    //"Sci-Fi Texture Pack 2 https://assetstore.unity.com/packages/2d/textures-materials/sci-fi-texture-pack-2-42026";
+    //"Standard Assets (for Unity 2018.4) https://assetstore.unity.com/packages/essentials/asset-packs/standard-assets-for-unity-2018-4-32351";
+}
