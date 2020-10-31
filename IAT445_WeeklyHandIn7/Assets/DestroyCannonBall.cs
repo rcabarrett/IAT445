2b3195a302f059794be1254d87ec92166522f6a8
Robin C. A. Barrett <rcbarret@sfu.ca>
2020-10-31 16:33:30 -0700

    Projectile lab handin

---
+++ IAT445_WeeklyHandIn7/Assets/DestroyCannonBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCannonBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Munition")
+        {
+            // Destroys Cannon Ball
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ IAT445_WeeklyHandIn7/Assets/FireCannon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireCannon : MonoBehaviour
+{
+
+    public Transform spawnPoint;
+    public Rigidbody Munition;
+    public float velocity = 3000;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody CannonBase;
+            // Spawns Cannon Ball
+            CannonBase = Instantiate(Munition, spawnPoint.position, spawnPoint.rotation) as Rigidbody;
+            CannonBase.AddForce(spawnPoint.forward * velocity);
+        }
+    }
+}
