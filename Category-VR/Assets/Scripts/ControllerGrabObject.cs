ac3f157b5f5e0474151549ec5a4471b535d18f0b
Robin C. A. Barrett <rcbarret@sfu.ca>
2020-11-05 11:40:28 -0800

    Upload first prototype
    Totally forgot to do version control for this so far! Big oof, but thankfully nothing went wrong

---
+++ Category-VR/Assets/Scripts/ControllerGrabObject.cs
@@ -0,0 +1,113 @@
+﻿// Script based on tutorial by Eric Van de Kerckhove available at:
+// https://www.raywenderlich.com/9189-htc-vive-tutorial-for-unity#toc-anchor-008
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ControllerGrabObject : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Behaviour_Pose controllerPose;
+    public SteamVR_Action_Boolean grabAction;
+
+    private GameObject collidingObject; // 1
+    private GameObject objectInHand; // 2
+
+    // This method accepts a collider as a parameter and uses its GameObject as the collidingObject for grabbing and releasing
+    private void SetCollidingObject(Collider col)
+    {
+        // 1
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+        // 2
+        collidingObject = col.gameObject;
+    }
+
+
+    // These methods handle what should happen when the trigger collider enters and exits another collider.
+    // 1
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // 2
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // 3
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+
+        collidingObject = null;
+    }
+
+    // Next, you’ll add some code to grab an object below OnTriggerExit()
+    private void GrabObject()
+    {
+        // 1
+        objectInHand = collidingObject;
+        collidingObject = null;
+        // 2
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    // 3
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+
+    // This next block handles releasing the object:
+    private void ReleaseObject()
+    {
+        // 1
+        if (GetComponent<FixedJoint>())
+        {
+            // 2
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+            // 3
+            objectInHand.GetComponent<Rigidbody>().velocity = controllerPose.GetVelocity();
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = controllerPose.GetAngularVelocity();
+
+        }
+        // 4
+        objectInHand = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grabAction.GetLastStateDown(handType))
+        {
+            if (collidingObject)
+            {
+                GrabObject();
+            }
+        }
+
+        // 2
+        if (grabAction.GetLastStateUp(handType))
+        {
+            if (objectInHand)
+            {
+                ReleaseObject();
+            }
+        }
+    }
+}
